b1bf4b4cbdc5c3c42b8af0424a1f5095762b57d4
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-08-08 16:39:24 -0400

    Completed

--- copyspecial.py
+++ copyspecial.py
@@ -12,30 +12,61 @@ import os
 import shutil
 import commands
 import argparse
+import zipfile
+from pprint import pprint
 
 """Copy Special exercise
 """
 
-# +++your code here+++
-# Write functions and modify main() to call them
+def get_special_paths(directory):
+    special_list = []
+    dirlist = os.listdir(directory)
+    for files in dirlist:
+        special = (str(re.findall(r"\w+__\w+__.\w+", files))[2:-2])
+        if len(special) >= 1:
+            pathway = os.path.abspath(special) 
+            special_list.append(pathway)
+    # print pathway
+    return special_list
 
-def main():
-    # This snippet will help you get started with the argparse module.
+def copy_to(paths, dir):
+    for path in paths:
+        if os.path.exists(dir):
+            shutil.copy(path,dir)
+        else:
+            os.makedirs(dir)
+            shutil.copy(path,dir)
+
+def zip_to(paths, zippath):
+    string = ""
+    with zipfile.ZipFile(zippath, 'w') as zip:
+        for file in paths:
+            string += " " +file
+            zip.write(file)
+        
+    command = "zip -j " + zippath + string
+    print "command im going to do: " 
+    print command
+
+def main(args = None):
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO need an argument to pick up 'from_dir'
+    parser.add_argument('fromdir', help='get all special files from whatever directory is specified after the --fromdir flag')
     args = parser.parse_args()
+    print args
+    special_files_list = get_special_paths('.')
+    if args.fromdir:
+         get_special_paths(args.fromdir)
 
-    # TODO you must write your own code to get the cmdline args.
-    # Read the docs and examples for the argparse module about how to do this.
+    if args.todir:
+        copy_to(special_files_list, args.todir)
+        print "copy success"
+        
+    if args.tozip:
+        zip_to(special_files_list, args.tozip)
 
-    # Parsing command line arguments is a must-have skill.
-    # This is input data validation.  If something is wrong (or missing) with any
-    # required args, the general rule is to print a usage message and exit(1).
-
-    # +++your code here+++
-    # Call your functions
-  
+   
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
